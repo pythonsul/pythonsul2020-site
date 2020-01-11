4f18f74e7e0b0f19802f7b6b50b4e4896e146c6b
Francisco Alface <falface@gmail.com>
2020-01-11 20:26:55 -0300

    Gerado gh-pages

--- publishconf.py
+++ publishconf.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
@@ -11,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'pythonsul.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
